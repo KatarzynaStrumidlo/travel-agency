91503eeaef95543ce8647bfa8281a57d1a574619
Katarzyna Strumidło <strumidlokd@gmail.com>
2021-04-16 12:47:51 +0200

    Fix test should not render div tags if props tags is falsy in TripSummary.test.js, add tests to OrderOption component

--- src/components/features/TripSummary/TripSummary.test.js
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -6,14 +6,7 @@ describe('Component TripSummary', () => {
   it('should render link to the correct address', () => {
     const expectedLinkId = 'abc';
     const expectedLink = '/trip/abc';
-    const component = shallow(<TripSummary
-      image='picture.jpg'
-      id={expectedLinkId}
-      days={2}
-      name='picture'
-      cost='20'
-      tags={[]}
-    />);
+    const component = shallow(<TripSummary id={expectedLinkId} />);
 
     expect(component.find('.link').prop('to')).toEqual(expectedLink);
   });
@@ -21,14 +14,7 @@ describe('Component TripSummary', () => {
   it('img has correct src and alt', () => {
     const expectedSrc = 'picture.jpg';
     const expectedAlt = 'picture';
-    const component = shallow(<TripSummary
-      image={expectedSrc}
-      id='abc'
-      days={2}
-      name={expectedAlt}
-      cost='20'
-      tags={[]}
-    />);
+    const component = shallow(<TripSummary image={expectedSrc} name={expectedAlt} />);
 
     expect(component.find('img').prop('src')).toEqual(expectedSrc);
     expect(component.find('img').prop('alt')).toEqual(expectedAlt);
@@ -38,14 +24,7 @@ describe('Component TripSummary', () => {
     const expectedDays = 2;
     const expectedName = 'name';
     const expectedCost = '20';
-    const component = shallow(<TripSummary
-      image='picture.jpg'
-      id='abc'
-      days={expectedDays}
-      name={expectedName}
-      cost={expectedCost}
-      tags={[]}
-    />);
+    const component = shallow(<TripSummary days={expectedDays} name={expectedName} cost={expectedCost} />);
 
     expect(component.find('.details').childAt(0).text()).toEqual(`${expectedDays} days`);
     expect(component.find('.title').text()).toEqual(expectedName);
@@ -54,30 +33,16 @@ describe('Component TripSummary', () => {
 
   it('should render array with elements in right order', () => {
     const expectedTags = ['nice', 'common', 'unexpected'];
-    const component = shallow(<TripSummary
-      image='picture.jpg'
-      id='abc'
-      days={2}
-      name='name'
-      cost='20'
-      tags={expectedTags}
-    />);
+    const component = shallow(<TripSummary  tags={expectedTags} />);
 
     expect(component.find('.tag').at(0).text()).toEqual(expectedTags[0]);
     expect(component.find('.tag').at(1).text()).toEqual(expectedTags[1]);
     expect(component.find('.tag').at(2).text()).toEqual(expectedTags[2]);
   });
 
-  it('should not render array if props tags is falsy', () => {
-    const component = shallow(<TripSummary
-      image='picture.jpg'
-      id='abc'
-      days={2}
-      name='name'
-      cost='20'
-    />);
+  it('should not render div tags if props tags is falsy', () => {
+    const component = shallow(<TripSummary />);
 
-    expect(component.find('.tag')).toBe(false);
+    expect(component.hasClass('tags')).toEqual(false);
   });
-
 });
--- src/components/layout/Hero/Hero.test.js
+++ src/components/layout/Hero/Hero.test.js
@@ -12,6 +12,7 @@ describe('Component Hero', () => {
   it('should throw error without required props', () => {
     expect(() => shallow(<Hero />)).toThrow();
   });
+
   it('should render correct title and image', () => {
     const expectedTitle = 'Lorem ipsum';
     const expectedImage = 'image.jpg';
@@ -21,6 +22,7 @@ describe('Component Hero', () => {
     expect(renderedTitle).toEqual(expectedTitle);
     expect(component.find('.image').prop('src')).toEqual(expectedImage);
   });
+
   it('renders correct classNames', () => {
     const mockVariants = 'small dummy';
     const component = shallow(<Hero titleText='Lorem' imageSrc='image.jpg' variant={mockVariants} />);
