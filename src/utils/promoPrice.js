90fe8137ddfd5dcf42146266a7153db4a79f78c9
Katarzyna Strumidło <strumidlokd@gmail.com>
2021-04-29 13:05:32 +0200

    Modify promoPrice function

--- src/utils/promoPrice.js
+++ src/utils/promoPrice.js
@@ -1,6 +1,6 @@
 export const promoPrice = (price, promo) => {
   if (typeof(price) === 'string' && price.includes('$')){
-    price = price.slice(1);
+    price = price.slice(1).replace(',', '');
     price = parseFloat(price);
     let happyHourPrice = price - (price * promo / 100);
     return ('$' + happyHourPrice);
